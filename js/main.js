8c6d67a7d64e3d1d5d0de6ece887e728f95e9325
Alexander <102146514+Alexxxlev@users.noreply.github.com>
2023-07-19 14:24:56 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,58 @@
-const menuBtn = document.querySelector('.menu__btn');
-const menuMobile = document.querySelector('.header__menu-list');
+// mobile menu
+const menuBtn = document.querySelector(".menu__btn");
+const menuMobile = document.querySelector(".header__menu-list");
 
-menuBtn.addEventListener('click', () => {
-  menuMobile.classList.toggle('menu--open');
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
+
+// feedback slider
+const swiperFeedback = new Swiper(".feedback__slider", {
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// certificates slider
+const swiperCertificates = new Swiper(".certificates__slider", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    1000: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+// accordeon
+const accordeon = document.querySelector(".accordeon");
+const accordeonTitles = accordeon.querySelectorAll(".accordeon__title");
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener("click", function () {
+    const currentText = accordeonTitle.parentElement.querySelector(
+      ".accordeon__content"
+    );
+    accordeonTitle.classList.toggle("accordeon__title--active");
+    currentText.classList.toggle("accordeon__content--visible");
+
+    if (currentText.classList.contains("accordeon__content--visible")) {
+      currentText.style.maxHeight = currentText.scrollHeight + "px";
+    } else {
+      currentText.style.maxHeight = null;
+    }
+  });
 });
